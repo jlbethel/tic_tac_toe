2d6d2e6eb1be6f74526ed1c62d6f68bd0032d57b
Kyle Pratuch & Jason Bethel <student@epicodus.com>
2015-09-16 11:46:08 -0700

    add Board and space assignment

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Player(playerNumber) {
   this.playerNumber = playerNumber;
 }
 
-Player.prototype.mark = function(playerNumber) {
+Player.prototype.mark = function() {
   if (this.playerNumber === 1) {
     return "X";
   } else {
@@ -25,4 +25,40 @@ Space.prototype.xCoordinate = function() {
 
 Space.prototype.yCoordinate = function() {
   return this.y;
+}
+
+Space.prototype.fullSpace = function() {
+  var stringX = this.x.toString();
+  var stringY = this.y.toString();
+  return stringX + stringY;
+}
+
+// Space.prototype.markedBy = function(player) {
+//   if (player.mark() = "X") {
+//     return this.player;
+//   }
+// }
+
+//Board object prototype:
+function Board() {
+  this.unclaimedSpaces = ["11", "12", "13", "21", "22", "23", "31", "32", "33"];
+  this.playerOneSpaces = [];
+  this.playerTwoSpaces = [];
+}
+
+Board.prototype.checkBoard = function() {
+  return this.unclaimedSpaces;
+}
+
+Board.prototype.assignSpace = function(Space, Player) {
+  var space = Space.fullSpace();
+  var player = Player.mark();
+  var foundSpace = this.unclaimedSpaces.indexOf(space);
+  var takenSpace = this.unclaimedSpaces.splice(foundSpace, 1);
+  if (player === "X") {
+    this.playerOneSpaces.push(takenSpace.join(""));
+  } else {
+    this.playerTwoSpaces.push(takenSpace.join(""));
+  };
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -14,6 +14,41 @@ describe('Space', function() {
 
   it("returns the yCoordinate", function() {
     var testSpace = new Space(1,2);
-    expect(testSpace.yCoordinate(2)).to.equal(2);
+    expect(testSpace.yCoordinate()).to.equal(2);
+  });
+
+  it("returns the fullSpace", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.fullSpace()).to.equal("12");
   });
 });
+
+describe('Board', function() {
+  it("returns a full board before any plays", function() {
+    var testBoard = new Board();
+    expect(testBoard.unclaimedSpaces).to.eql(["11", "12", "13", "21", "22", "23", "31", "32", "33"]);
+    expect(testBoard.playerOneSpaces).to.eql([]);
+    expect(testBoard.playerTwoSpaces).to.eql([]);
+  });
+
+  it("returns an updated board", function() {
+    var testBoard = new Board();
+    var testSpace = new Space(1,2);
+    var testPlayer = new Player(1);
+    testBoard.assignSpace(testSpace, testPlayer);
+    expect(testBoard.unclaimedSpaces).to.eql(["11", "13", "21", "22", "23", "31", "32", "33"]);
+    expect(testBoard.playerOneSpaces).to.eql(["12"]);
+    expect(testBoard.playerTwoSpaces).to.eql([]);
+  });
+
+});
+
+
+
+//   it("lets a player mark a space", function() {
+//     var testPlayer = new Player(1);
+//     var testSpace = new Space(1,2);
+//     testSpace.markedBy(testPlayer);
+//     expect(testSpace.markedBy()).to.equal(testPlayer);
+//   });
+// });
